089c088bcaa7363984d7bf259324467ecfb6ab2e
Vedran Linić <toolrijeka@gmail.com>
2019-03-31 21:17:15 +0200

    format document

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const phaseUl = phrase.querySelector('ul');
-let missed = 0 ;
+let missed = 0;
 const ovarlay = document.getElementById('overlay');
 const startGame = document.getElementsByClassName('btn__reset')[0];
 
@@ -14,20 +14,20 @@ const phrases = [
 ];
 
 //create function to get random phase array and then split it to single letters
-function getRandomPhraseAsArray(arr){
+function getRandomPhraseAsArray(arr) {
     const randomPhase = arr[Math.floor(Math.random() * arr.length)];
     const letters = randomPhase.split("");
     return letters;
 }
 
 //create function to add phase to display
-function addPhraseToDisplay(arr){
-    for(let i = 0; i < arr.length; i += 1){
+function addPhraseToDisplay(arr) {
+    for (let i = 0; i < arr.length; i += 1) {
         let letter = arr[i];
         const li = document.createElement('li');
-        if (letter === ' '){
-            li.className = 'space';            
-        }else {
+        if (letter === ' ') {
+            li.className = 'space';
+        } else {
             li.className = 'letter';
         }
         phaseUl.appendChild(li);
@@ -35,22 +35,22 @@ function addPhraseToDisplay(arr){
 }
 
 //create check letter function
-function checkLetter(button){
+function checkLetter(button) {
     let letters = document.getElementsByClassName(letter);
-    for (let i = 0; i < letters.length; i+=1){
-        if (letters[i] === button){
+    for (let i = 0; i < letters.length; i += 1) {
+        if (letters[i] === button) {
             letters[i].className = 'show';
-        }else{
+        } else {
             missed += 1;
         }
     }
 }
 
 //create CheckWin function
-function CheckWin(){};
+function CheckWin() { };
 
 
-startGame.addEventListener('click', ()=>{    
+startGame.addEventListener('click', () => {
     overlay.style.display = 'none';
     //appand phase to screen
     const phrase = getRandomPhraseAsArray(phrases)
